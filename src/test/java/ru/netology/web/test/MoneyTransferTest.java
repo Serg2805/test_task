c70dd8f3d2506b67ac27a37b637c715887def4eb
Sergei <91976559+Serg2805@users.noreply.github.com>
2023-07-20 20:43:57 +0300

    Update MoneyTransferTest.java
    change test

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,43 +1,35 @@
 package ru.netology.web.test;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
-import ru.netology.web.page.LoginPageV1;
-import ru.netology.web.page.LoginPageV2;
-import ru.netology.web.page.LoginPageV3;
+import ru.netology.web.page.DashboardPage;
+import ru.netology.web.page.LoginPage;
+import ru.netology.web.page.TransferPage;
 
 import static com.codeborne.selenide.Selenide.open;
+
+
 
 class MoneyTransferTest {
     @Test
     void shouldTransferMoneyBetweenOwnCardsV1() {
       open("http://localhost:9999");
-      var loginPage = new LoginPageV1();
+      var loginPage = new LoginPage();
 //    var loginPage = open("http://localhost:9999", LoginPageV1.class);
       var authInfo = DataHelper.getAuthInfo();
       var verificationPage = loginPage.validLogin(authInfo);
       var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
       verificationPage.validVerify(verificationCode);
+      var dashboardPage = new DashboardPage();
+      var transferPage = new TransferPage();
+      int initialBalance = dashboardPage.getCardBalance("0");
+      transferPage.transferMoney("5559 0000 0000 0002", 1000);
+      int updatedBalance = dashboardPage.getCardBalance("0");
+      Assert.assertEquals(initialBalance - 1000, updatedBalance);
     }
 
-  @Test
-  void shouldTransferMoneyBetweenOwnCardsV2() {
-    open("http://localhost:9999");
-    var loginPage = new LoginPageV2();
-//    var loginPage = open("http://localhost:9999", LoginPageV2.class);
-    var authInfo = DataHelper.getAuthInfo();
-    var verificationPage = loginPage.validLogin(authInfo);
-    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
-  }
-
-  @Test
-  void shouldTransferMoneyBetweenOwnCardsV3() {
-    var loginPage = open("http://localhost:9999", LoginPageV3.class);
-    var authInfo = DataHelper.getAuthInfo();
-    var verificationPage = loginPage.validLogin(authInfo);
-    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
-  }
 }
+
+
 
